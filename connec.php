b88e1207dd3d4d320d0cc5e4d0104410d9943ab4
azizsahli <32931164+azizsahli@users.noreply.github.com>
2018-01-19 22:21:10 +0100

    Update connec.php

--- connec.php
+++ connec.php
@@ -2,15 +2,23 @@
 $servername = " phpMyAdmin demo";
 $username = "root";
 $password = "";
+$dbname = "facebook";
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=facebook", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
-    }
-catch(PDOException $e)
-    {
-    echo "Connection failed: " . $e->getMessage();
-    }
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO accounts (id, pass,)
+VALUES ('John', 'Doe')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?> 
